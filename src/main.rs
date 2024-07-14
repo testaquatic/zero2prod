5b58eca2e3ba587124eeb7370b7f899af46ba9d0
Your Name <you@example.com>
2024-07-15 02:50:48 +0900

    tracing 도입2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-use env_logger::Env;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::{configuration::Settings, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // `init`는 `set_logger`를 호출한다.
-    // 다른 작업은 필요하지 않다.
-    // RUST_LOG 환경 변수가 설정되이 있지 않으면 info 및 그 이상의 레벨의 모든 로그를 출력한다.
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // 이전에 있던 `env_logger` 행을 제거했다.
+
+    // 모든 `log`의 이벤트를 구독자에게 리다이렉팅 한다.
+    LogTracer::init().expect("Failed to set logger.");
+
+    // RUST_LOG 환경 변수가 설정되어 있지 않으면 info레벨 및 그 이상의 모든 span을 출력한다.
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("info"));
+    // 포맷이 적용된 span들을 stdout으로 출력한다.
+    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
+    // `with` 메서드는 `SubscriberExt`에서 제공한다.
+    // `SubscriberExt`는 `Subscriber`의 확장 트레이트이며, `tracing_subscriber`에 의해 노출된다.
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    // 애플리케이션에서 `set_global_default`를 사용해서 span을 처리하기 위해 어떤 subscriber를 사용해야 하는지 지정할 수 있다.
+    set_global_default(subscriber).expect("Failed to set subscriber.");
+
     // 구성을 읽을 수 없으면 패닉에 빠진다.
     let configuration = Settings::get_configuration().expect("Failed to read configuration.");
     let pg_pool = configuration
